21e77b6f97b44ef667b24083a849442784da739b
Vladimir Minkin <vladimir.minkin@gmail.com>
2023-04-24 22:31:42 +0300

    CellRegions simplified

--- lib/regions.dart
+++ lib/regions.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:workspace_layout/cell/layout_cell.dart';
 
+enum CellRegionSide { TOP, RIGHT, BOTTOM, LEFT, CENTER }
+
+const FULL_HEIGHT_SIDES = [CellRegionSide.RIGHT, CellRegionSide.LEFT, CellRegionSide.CENTER];
+
 class LayoutRegions extends StatefulWidget {
   const LayoutRegions(
     this.cell, {
@@ -13,15 +17,11 @@ class LayoutRegions extends StatefulWidget {
   State<LayoutRegions> createState() => _LayoutRegionsState();
 }
 
-enum CellRegionSide { TOP, RIGHT, BOTTOM, LEFT, CENTER }
-
 class _LayoutRegionsState extends State<LayoutRegions> {
   final ValueNotifier<CellRegionSide?> _side = ValueNotifier(null);
   Size? _size;
 
-  final _FULL_HEIGHT_SIDES = [CellRegionSide.RIGHT, CellRegionSide.LEFT, CellRegionSide.CENTER];
-
-  void onHover(Size? size, CellRegionSide? side) {
+  void onInside(Size size, CellRegionSide? side) {
     _size = size;
     _side.value = side;
   }
@@ -37,7 +37,7 @@ class _LayoutRegionsState extends State<LayoutRegions> {
             final currentSize = (context.findRenderObject() as RenderBox).size;
             double? top = CellRegionSide.BOTTOM != side ? 0 : currentSize.height - _size!.height;
             double? left = CellRegionSide.RIGHT != side ? 0 : currentSize.width - _size!.width;
-            final isFullCellHeight = _FULL_HEIGHT_SIDES.contains(side);
+            final isFullCellHeight = FULL_HEIGHT_SIDES.contains(side);
             return Positioned(
               top: top,
               left: left,
@@ -46,6 +46,7 @@ class _LayoutRegionsState extends State<LayoutRegions> {
                 height: isFullCellHeight ? currentSize.height : _size!.height,
                 alignment: Alignment.center,
                 padding: const EdgeInsets.all(2),
+                color: Colors.black.withAlpha(80),
                 child: Container(
                   decoration: BoxDecoration(
                     border: Border.all(color: Colors.black.withAlpha(90), width: 2),
@@ -55,92 +56,76 @@ class _LayoutRegionsState extends State<LayoutRegions> {
             );
           },
         ),
-        Column(
-          children: [
-            LayoutCellRegion(
-              CellRegionSide.TOP,
-              onHover: onHover,
-            ),
-            Expanded(
-              flex: 3,
-              child: Row(
-                children: [
-                  LayoutCellRegion(
-                    CellRegionSide.LEFT,
-                    onHover: onHover,
-                  ),
-                  LayoutCellRegion(
-                    flex: 3,
-                    CellRegionSide.CENTER,
-                    onHover: onHover,
-                  ),
-                  LayoutCellRegion(
-                    CellRegionSide.RIGHT,
-                    onHover: onHover,
-                  ),
-                ],
+        CellRegions(onInside),
+      ],
+    );
+  }
+}
+
+class CellRegions extends StatelessWidget {
+  const CellRegions(this.onInside, {Key? key}) : super(key: key);
+
+  final Function(Size, CellRegionSide?) onInside;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        LayoutCellRegion(
+          CellRegionSide.TOP,
+          onInside: onInside,
+        ),
+        Expanded(
+          flex: 3,
+          child: Row(
+            children: [
+              LayoutCellRegion(
+                CellRegionSide.LEFT,
+                onInside: onInside,
               ),
-            ),
-            LayoutCellRegion(
-              CellRegionSide.BOTTOM,
-              onHover: onHover,
-            ),
-          ],
+              LayoutCellRegion(
+                flex: 3,
+                CellRegionSide.CENTER,
+                onInside: onInside,
+              ),
+              LayoutCellRegion(
+                CellRegionSide.RIGHT,
+                onInside: onInside,
+              ),
+            ],
+          ),
+        ),
+        LayoutCellRegion(
+          CellRegionSide.BOTTOM,
+          onInside: onInside,
         ),
       ],
     );
   }
 }
 
-class LayoutCellRegion extends StatefulWidget {
+class LayoutCellRegion extends StatelessWidget {
   const LayoutCellRegion(
     this.side, {
     super.key,
     this.flex = 1,
-    this.baseColor,
-    required this.onHover,
+    required this.onInside,
   });
 
   final int flex;
   final CellRegionSide side;
-  final Color? baseColor;
-  final Function(Size?, CellRegionSide?) onHover;
-
-  @override
-  State<LayoutCellRegion> createState() => _LayoutCellRegionState();
-}
-
-class _LayoutCellRegionState extends State<LayoutCellRegion> {
-  Color? color;
-
-  @override
-  void initState() {
-    super.initState();
-    color = widget.baseColor;
-  }
+  final Function(Size, CellRegionSide?) onInside;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      flex: widget.flex,
+      flex: flex,
       child: MouseRegion(
-        onEnter: (_) {
-          setState(() {
-            color = Colors.amber[100]!.withAlpha(80);
-            final size = (context.findRenderObject() as RenderBox).size;
-            widget.onHover(size, widget.side);
-          });
-        },
-        onExit: (_) {
-          setState(() {
-            color = widget.baseColor;
-            widget.onHover(null, null);
-          });
-        },
-        child: Container(
+        onEnter: (_) => onInside((context.findRenderObject() as RenderBox).size, side),
+        onExit: (_) => onInside((context.findRenderObject() as RenderBox).size, null),
+        child: const SizedBox(
           width: double.maxFinite,
           height: double.maxFinite,
-          color: color,
         ),
       ),
     );
